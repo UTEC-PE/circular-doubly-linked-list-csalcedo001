c29d845f0561f4fdeab96e104b538c800b2847e6
César Salcedo <cesar.salcedo@utec.edu.pe>
2018-08-29 00:41:05 -0500

    Test

--- Makefile
+++
@@ -1,4 +0,0 @@
-all: main
-
-main: main.cpp list.h iterator.h node.h
-	g++ -std=c++11 main.cpp -o main
--- iterator.h
+++ iterator.h
@@ -7,37 +7,16 @@ template <typename T>
 class Iterator {
     private:
         Node<T>* current;
-
+    
     public:
-        Iterator() {
-            current = nullptr;
-        };
-        Iterator(Node<T>* node) {
-            current = node;
-        }
-
-        Iterator<T> operator=(Iterator<T> node) {
-            current = node.current;
-            return current;
-        }
-        bool operator!=(Iterator<T> cmp) {
-            return current != cmp.current;
-        }
-        Iterator<T> operator++() {
-            if (current)
-                current = current->next;
-
-            return *this;
-        }
-        Iterator<T> operator--() {
-            if (current)
-                current = current->prev;
-
-            return *this;
-        }
-        T operator*() {
-            return current->data;
-        }
+        Iterator();
+        Iterator(Node<T>* node);
+         
+        Iterator<T> operator=(Iterator<T> node);
+        bool operator!=(Iterator<T> cmp);
+        Iterator<T> operator++();
+        Iterator<T> operator--();
+        T operator*();
 };
 
 #endif
